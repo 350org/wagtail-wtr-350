c1eaff2795b8b0d6ae86e821555c032cc805b79c
Sukhada Kulkarni <sukhada91@gmail.com>
2026-09-01 12:53:25 -0400

    feat: set AK country input based on Cloudflare country code

--- static_src/js/main.js
+++ static_src/js/main.js
@@ -3,10 +3,12 @@ import MobileMenu from './components/mobile-menu.js';
 import Accordion from './components/accordion.js';
 import FormAjax from './components/form-ajax.js';
 import RegionalSiteModal from './components/regional-site-modal.js';
+import ActionKitCountryPrefill from './components/actionkit-country-prefill.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     MobileMenu.init();
     Accordion.init();
     FormAjax.init();
     RegionalSiteModal.init();
+    ActionKitCountryPrefill.init();
 });
