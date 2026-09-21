6c5a4aaab0efa683683a78d207b42c5f6eea64cf
Sukhada Kulkarni <sukhada91@gmail.com>
2026-09-21 07:49:51 -0700

    chore: add document link to card content

--- wagtail_wtr/settings/base.py
+++ wagtail_wtr/settings/base.py
@@ -96,6 +96,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
                 "wagtail.contrib.settings.context_processors.settings",
                 "wtrx.context_processors.usercentrics",
                 "wtrx.context_processors.google_sso",
